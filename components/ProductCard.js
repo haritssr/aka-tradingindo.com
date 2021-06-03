d2055b8f50acce8c8e13cf5a2ead1480ab5ca6ac
Harits Syah <haritssr@gmail.com>
2021-06-04 06:41:46 +0700

    a

--- components/ProductCard.js
+++ components/ProductCard.js
@@ -1,24 +1,21 @@
+import Link from "next/link";
+import Image from "next/image";
+
 export default function ProductCard(props) {
-    return (
-      <>
-        <Link href={`${props.product}`}>
-          <div className="w-5/6 h-auto m-5 mx-auto cursor-pointer ">
-            <div>
-              <Image
-                
-                src={`${props.image}`}
-                width={1080}
-                height={705}
-                priority
-              />
-            </div>
-            <div className="flex flex-col px-3 py-4 -mt-2 cursor-pointer bg-green-1000 hover:bg-opacity-70">
-              <div className="px-2 text-2xl font-bold text-left text-white font-EBGaramond dark:text-white">
-                {props.productName}
-              </div>
+  return (
+    <>
+      <Link href={`${props.product}`}>
+        <div className="w-5/6 h-auto m-5 mx-auto cursor-pointer ">
+          <div>
+            <Image src={`${props.image}`} width={1080} height={705} priority />
+          </div>
+          <div className="flex flex-col px-3 py-4 -mt-2 cursor-pointer bg-green-1000 hover:bg-opacity-70">
+            <div className="px-2 text-2xl font-bold text-left text-white font-EBGaramond dark:text-white">
+              {props.productName}
             </div>
           </div>
-        </Link>
-      </>
-    );
-  }
+        </div>
+      </Link>
+    </>
+  );
+}
